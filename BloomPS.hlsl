a613ac41d6c5c7be51387120705cd365dc15cc96
Jen <notyouravgjen@gmail.com>
2023-08-01 10:33:12 -0700

    code cleanup

--- BloomPS.hlsl
+++ BloomPS.hlsl
@@ -2,7 +2,6 @@
 
 // The texture that shades onto the surface
 Texture2D myTexture : register(t0);
-//Texture2D myTexture2 : register(t1);
 SamplerState mySampler : register(s0);
 
 // Defines the input to this pixel shader
@@ -22,22 +21,22 @@ float4 main(VertexToPixel input) : SV_TARGET
 	// Source: http://www.rastertek.com/dx11tut36.html
 	// To create the blurred pixel we first set the color to black and then we add the center pixel and the eight neighbors to the final color based on the weight of each
 	
-	 // Create the weights that each neighbor pixel will contribute to the blur.
-    float weight0 = 1.0f;
-    float weight1 = 0.9f;
-    float weight2 = 0.55f;
-    float weight3 = 0.18f;
-    float weight4 = 0.1f;
+	// Create the weights that each neighbor pixel will contribute to the blur.
+	float weight0 = 1.0f;
+	float weight1 = 0.9f;
+	float weight2 = 0.55f;
+	float weight3 = 0.18f;
+ 	float weight4 = 0.1f;
 
-    // Create a normalized value to average the weights out a bit.
-    float normalization = (weight0 + 2.0f * (weight1 + weight2 + weight3 + weight4));
+	// Create a normalized value to average the weights out a bit.
+	float normalization = (weight0 + 2.0f * (weight1 + weight2 + weight3 + weight4));
 
-    // Normalize the weights.
-    weight0 = weight0 / normalization;
-    weight1 = weight1 / normalization;
-    weight2 = weight2 / normalization;
-    weight3 = weight3 / normalization;
-    weight4 = weight4 / normalization;
+	// Normalize the weights.
+	weight0 = weight0 / normalization;
+	weight1 = weight1 / normalization;
+	weight2 = weight2 / normalization;
+	weight3 = weight3 / normalization;
+	weight4 = weight4 / normalization;
 
 	// 1 2 3
 	// 4(5)6
@@ -59,14 +58,14 @@ float4 main(VertexToPixel input) : SV_TARGET
 	finalColor += myTexture.Sample(mySampler, tc2) * weight3;
 	finalColor += myTexture.Sample(mySampler, tc3) * weight2;
 	finalColor += myTexture.Sample(mySampler, tc4) * weight1;
-    finalColor += myTexture.Sample(mySampler, tc5) * weight0; // center pixel
+	finalColor += myTexture.Sample(mySampler, tc5) * weight0; // center pixel
 	finalColor += myTexture.Sample(mySampler, tc6) * weight1;
 	finalColor += myTexture.Sample(mySampler, tc7) * weight2;
 	finalColor += myTexture.Sample(mySampler, tc8) * weight3;
 	finalColor += myTexture.Sample(mySampler, tc9) * weight4;
 
 
-	// todo: additive blending
+	// TODO: additive blending
 
 	finalColor.a = 1.0f;
 	return finalColor;
--- BrightExtractionPS.hlsl
+++ BrightExtractionPS.hlsl
@@ -8,16 +8,7 @@ float4 main(float4 color : COLOR0, float2 texCoord : TEXCOORD0) : SV_Target0
     float4 textureColor = Texture.Sample(TextureSampler, texCoord); // sample the pixel color
 	float4 finalColor = {0, 0, 0, 1};
 
-	// "bright" is currently defined as grey/white pixels
-	//if(textureColor[0] > 0.25 && textureColor[1] > 0.25 && textureColor[2] > 0.25)
-
-	// "bright" is currently defined as grey/white pixels
-	/*if(textureColor[0] + textureColor[1] + textureColor[2] > 1.25)
-	{
-		finalColor = textureColor;
-	}
-	*/
-	//HSL version: Brightness based on Lightness
+	// Calculate brightness from HSL
 	float high, low;
 	if(textureColor[0] > textureColor[1]){
 		high = textureColor[0];
@@ -35,39 +26,10 @@ float4 main(float4 color : COLOR0, float2 texCoord : TEXCOORD0) : SV_Target0
 		low = textureColor[2];
 	}
 
-	if(1/high + 1/low > .9)//Play around with that
+	if(1/high + 1/low > .9)
 	{
 		finalColor = textureColor;
 	}
 
 	return finalColor;
 }
-
-/*
-// The texture that shades onto the surface
-Texture2D myTexture : register(t0);
-SamplerState mySampler : register(s0);
-
-// Defines the input to this pixel shader
-// - Should match the output of our corresponding vertex shader
-struct VertexToPixel
-{
-	float4 position		: SV_POSITION;
-	float2 texcoord		: TEXCOORD;
-};
-
-// Entry point for this pixel shader
-float4 main(VertexToPixel input) : SV_TARGET
-{
-	float4 textureColor = myTexture.Sample(mySampler, input.texcoord); // sample the pixel color
-	float4 finalColor = {0, 0, 0, 1};
-
-	// "bright" is currently defined as grey/white pixels
-	if(textureColor[0] > 0.25 && textureColor[1] > 0.25 && textureColor[2] > 0.25)
-	{
-		finalColor = textureColor;
-	}
-
-	return finalColor;
-}
-*/
